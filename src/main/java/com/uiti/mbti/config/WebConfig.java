dd2c617f32a8aa21f04eede4d81bd2a0c0ad0297
Seokje Park <psj.uiti@gmail.com>
2022-01-24 17:48:44 +0900

    cors..3000 allow headers, methods

--- src/main/java/com/uiti/mbti/config/WebConfig.java
+++ src/main/java/com/uiti/mbti/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
