7c86acbf355759bab378e52d4eac1621b2060f8a
SeokjePark <psj.uiti@gmail.com>
2022-01-26 19:52:40 +0900

    another server

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-docker-compose down
-git checkout main
-git fetch --all
-git reset --hard origin/main
-git pull origin main
-mvn clean package -DskipTests
-docker-compose build --no-cache
+function trap_ctrlc() {
+	echo "Shutting down docker."
+	sudo docker-compose down
+	echo "Stopped bash opeation."
+	exit 2
+}
+
+trap "trap_ctrlc" 2
+
+mvn clean package -DskipTests &&
 docker-compose up -d
